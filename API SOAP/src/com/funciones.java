f1dbebe2bd63e8627e0ffa295f7d4d6b32878f9a
AlanSZK <54686091+AlanSZK@users.noreply.github.com>
2020-09-21 09:16:10 -0300

    Add files via upload

---
+++ API SOAP/src/com/funciones.java
@@ -0,0 +1,76 @@
+package com;
+
+public class funciones {
+	public static String nombrePropio(String nombres, String apPaterno, String apMaterno, char genero)
+	{
+		String resultado = null;
+		String mayuscula;
+		
+		//codigo para nombres
+		
+		nombres = nombres.toLowerCase();
+		
+		for(int i=0; i<nombres.length();i++)
+		{
+			if(i==0 )
+			{
+				mayuscula = String.valueOf(nombres.charAt(i));
+				mayuscula = mayuscula.toUpperCase();
+				
+				nombres = nombres.replaceFirst(String.valueOf(nombres.charAt(i)), mayuscula);
+				
+				
+			}
+			else if (nombres.charAt(i-1) ==' ')
+			{
+				mayuscula = String.valueOf(nombres.charAt(i));
+				mayuscula = mayuscula.toUpperCase();
+				
+				nombres = nombres.replaceFirst(String.valueOf(nombres.charAt(i)), mayuscula);
+				
+		
+			}
+		
+		}
+		
+		//codigo para apellidos
+		
+		apPaterno = apPaterno.toLowerCase();
+		apMaterno = apMaterno.toLowerCase();
+		
+		mayuscula = String.valueOf(apPaterno.charAt(0));
+		mayuscula = mayuscula.toUpperCase();
+		apPaterno = apPaterno.replaceFirst(String.valueOf(apPaterno.charAt(0)), mayuscula);
+		
+		mayuscula = String.valueOf(apMaterno.charAt(0));
+		mayuscula = mayuscula.toUpperCase();
+		apMaterno = apMaterno.replaceFirst(String.valueOf(apMaterno.charAt(0)), mayuscula);
+		
+		//creacion del saludo
+		if (genero == 'M')
+		{
+			resultado="Sr. ";
+		}
+		else
+		{
+			resultado="Sra. ";
+		}
+		
+		resultado+=nombres;
+		resultado+=" ";
+		resultado+=apPaterno;
+		resultado+=" ";
+		resultado+=apMaterno;
+		
+		
+		
+		return resultado;
+	}
+
+	public static void main (String[] args) 
+	{
+		String nombre = nombrePropio("aLAN jACEK","sLaZak","cASTRO",'M');
+		
+		System.out.println(nombre);
+	}
+}
