653277b3204c6302a332a2810fcfae309fa8461d
Diego Nuñez <diego.nunezg@utem.cl>
2020-09-24 18:09:25 -0300

    Funcion validacion rut

--- API SOAP/src/com/funciones.java
+++ API SOAP/src/com/funciones.java
@@ -66,11 +66,53 @@ public class funciones {
 		
 		return resultado;
 	}
+	
+	
+	
+	String rut;
+	public String getrut() {
+		return rut;
+	}
+	
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+	
+	
+	public static boolean validarRut(String rut) {
+
+		boolean validacion = false;
+		try {
+			
+			rut =  rut.toUpperCase();
+			rut = rut.replace(".", "");
+			rut = rut.replace("-", "");
+			int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+			char dv = rut.charAt(rut.length() - 1);
+
+			int m = 0, s = 1;
+			for (; rutAux != 0; rutAux /= 10) {
+				s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+			}
+			if (dv == (char) (s != 0 ? s + 47 : 75)) {
+				validacion = true;
+			}
+
+		} 
+		catch (java.lang.NumberFormatException e) {
+		}
+		catch (Exception e) {
+		}
+		return validacion;
+		}
 
 	public static void main (String[] args) 
 	{
 		String nombre = nombrePropio("aLAN jACEK","sLaZak","cASTRO","M");
-		
 		System.out.println(nombre);
+		System.out.print(validarRut("19.748.419-5"));
+		
+		
 	}
 }
