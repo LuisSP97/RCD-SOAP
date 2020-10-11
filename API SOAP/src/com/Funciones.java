57a6f0e023bafd8fd369a9d8866f362fc59adf44
Alan Slazak <alan.slazakc@utem.cl>
2020-10-11 15:07:21 -0300

    Correcciones de codigo (SonarScanner)

--- API SOAP/src/com/Funciones.java
+++ API SOAP/src/com/Funciones.java
@@ -1,6 +1,10 @@
 package com;
 
-public class funciones {
+public class Funciones {
+
+	
+	
+	
 	public static String nombrePropio(String nombres, String apPaterno, String apMaterno, String genero)
 	{
 		String resultado = null;
@@ -21,15 +25,14 @@ public class funciones {
 				
 				
 			}
-			else if (nombres.charAt(i-1) ==' ')
+			else if ( nombres.charAt(i-1) ==' ')
 			{
 				mayuscula = String.valueOf(nombres.charAt(i));
 				mayuscula = mayuscula.toUpperCase();
 				
 				nombres = nombres.replaceFirst(String.valueOf(nombres.charAt(i)), mayuscula);
-				
-		
 			}
+			
 		
 		}
 		
@@ -47,13 +50,17 @@ public class funciones {
 		apMaterno = apMaterno.replaceFirst(String.valueOf(apMaterno.charAt(0)), mayuscula);
 		
 		//creacion del saludo
-		if (genero.equals("M"))
+		if (genero.equals("M") || genero.equals("m"))
 		{
 			resultado="Sr. ";
 		}
-		else
+		else if (genero.equals("F") || genero.equals("f"))
 		{
 			resultado="Sra. ";
+		}
+		else
+		{
+			resultado="?. ";
 		}
 		
 		resultado+=nombres;
@@ -67,10 +74,53 @@ public class funciones {
 		return resultado;
 	}
 
-	public static void main (String[] args) 
-	{
-		String nombre = nombrePropio("aLAN jACEK","sLaZak","cASTRO","M");
-		
-		System.out.println(nombre);
+	String rut;
+	public String getrut() {
+		return rut;
 	}
+	
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+	
+	
+	public static boolean validarRut(String rut) {
+
+		boolean validacion = false;
+		try {
+			
+			rut =  rut.toUpperCase();
+			rut = rut.replace(".", "");
+			rut = rut.replace("-", "");
+			int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+			char dv = rut.charAt(rut.length() - 1);
+			
+
+			int m = 0;
+			int s = 1;
+			for (; rutAux != 0; rutAux /= 10) {
+				s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+			}
+			if (dv == (s != 0 ? s + 47 : 75)) {
+				validacion = true;
+			}
+
+		} 
+		catch (java.lang.NumberFormatException e) {
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return validacion;
+	}
+	
+	public void main (String[] args)
+	{
+		
+	}
+	
+	
+	
+
 }
